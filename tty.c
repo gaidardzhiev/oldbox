eeda6bf93be4f5658719742b886f7d387fc4438d
1v4n <42386354+gaidardzhiev@users.noreply.github.com>
2024-10-20 11:35:29 +0300

    Delete tty.c

--- tty.c
+++
@@ -1,4 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-char*ttyname();int main(argc,argv)char**argv;{register char *p;p=ttyname(0);if(argc==2&&!strcmp(argv[1],"-s"));else printf("%s\n",(p?p:"not a tty"));exit(p?0:1);}
