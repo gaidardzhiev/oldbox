5e99acfc242d82b4f2ed370b22346f9a6b25f1eb
gaidardzhiev <1v4n@protonmail.com>
2024-11-07 11:32:13 +0200

    Thu Nov  7 09:32:13 AM UTC 2024

--- ls.c
+++ ls.c
@@ -11,21 +11,17 @@ void ls(const char *path)
 	struct dirent *entry;
 	struct stat fileStat;
 	DIR *dp = opendir(path);
-
 	if (dp == NULL) {
 		perror("opendir");
 		return;
 	}
-
 	while ((entry = readdir(dp)) != NULL) {
 		char fullPath[2048];
 		snprintf(fullPath, sizeof(fullPath), "%s/%s", path, entry->d_name);
-
 		if (stat(fullPath, &fileStat) < 0) {
 			perror("stat");
 			continue;
 		}
-
 		printf((S_ISDIR(fileStat.st_mode)) ? "d" : "_");
 		printf((fileStat.st_mode & S_IRUSR) ? "r" : "_");
 		printf((fileStat.st_mode & S_IWUSR) ? "w" : "_");
@@ -37,14 +33,12 @@ void ls(const char *path)
 		printf((fileStat.st_mode & S_IWOTH) ? "w" : "_");
 		printf((fileStat.st_mode & S_IXOTH) ? "x" : "_");
 		printf(" %ld ", fileStat.st_nlink);
-
 		struct passwd *pw = getpwuid(fileStat.st_uid);
 		struct group  *gr = getgrgid(fileStat.st_gid);
 		printf("%s %s ", pw->pw_name, gr->gr_name);
 		printf("%lld ", (long long)fileStat.st_size);
 		printf("	%s\n", entry->d_name);
 	}
-
 	closedir(dp);
 }
 
