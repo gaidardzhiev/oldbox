6728589d6fe6e55428db48d53e315e1c554402be
gaidardzhiev <1v4n@protonmail.com>
2024-11-15 12:20:21 +0200

    Fri Nov 15 10:20:20 AM UTC 2024

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CC=tcc
 
-BINS=kill tty touch echo mount sync umount nice sleep tee wc yes rev ln cp basename true printf tr cat shell pwd ps grep du rm ascii2hex hexdump false replace readelf strings ls
+BINS=kill tty touch echo mount sync umount nice sleep tee wc yes rev ln cp basename true printf tr cat shell pwd ps grep du rm ascii2hex hexdump false replace readelf strings ls xoda
 all: $(BINS)
 
 $(BINS): %: %.c
@@ -44,3 +44,4 @@ man:
 	echo readelf.c - displays information about ELF files
 	echo strings.c - prints sequences of printable characters
 	echo ls.c - lists directory contents
+	echo xoda.c - dumps files in hex octal decimal or ASCII
---
+++ xoda.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define BUFFER_SIZE 16
+
+void print_hex(unsigned char *buffer, size_t bytes, size_t offset)
+{
+	printf("%08lx: ", offset);
+	for (size_t i = 0; i < bytes; i++) {
+		printf("%02x ", buffer[i]);
+	}
+	printf("\n");
+}
+
+void print_octal(unsigned char *buffer, size_t bytes, size_t offset)
+{
+	printf("%08lx: ", offset);
+	for (size_t i = 0; i < bytes; i++) {
+		printf("%03o ", buffer[i]);
+	}
+	printf("\n");
+}
+
+void print_decimal(unsigned char *buffer, size_t bytes, size_t offset)
+{
+	printf("%08lx: ", offset);
+	for (size_t i = 0; i < bytes; i++) {
+		printf("%03d ", buffer[i]);
+	}
+	printf("\n");
+}
+
+void print_ascii(unsigned char *buffer, size_t bytes, size_t offset)
+{
+	printf("%08lx: ", offset);
+	for (size_t i = 0; i < bytes; i++) {
+		if (isprint(buffer[i])) {
+			printf("%c", buffer[i]);
+		} else {
+			printf(".");
+		}
+	}
+	printf("\n");
+}
+
+void xoda(const char *filename, const char format)
+{
+	FILE *file = fopen(filename, "rb");
+	if (!file) {
+		perror("error opening file");
+		return;
+	}
+
+	unsigned char buffer[BUFFER_SIZE];
+	size_t bytesRead;
+	size_t offset = 0;
+
+	while ((bytesRead = fread(buffer, 1, BUFFER_SIZE, file)) > 0) {
+		switch (format) {
+		case 'x':
+			print_hex(buffer, bytesRead, offset);
+			break;
+		case 'o':
+			print_octal(buffer, bytesRead, offset);
+			break;
+		case 'd':
+			print_decimal(buffer, bytesRead, offset);
+			break;
+		case 'a':
+			print_ascii(buffer, bytesRead, offset);
+			break;
+		default:
+			fprintf(stderr, "invalid format specified\n");
+			fclose(file);
+			return;
+		}
+		offset += bytesRead;
+	}
+
+	fclose(file);
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3) {
+		fprintf(stderr, "usage: %s <filename> <x|o|d|a>\n", argv[0]);
+		return EXIT_FAILURE;
+	}
+
+	xoda(argv[1], argv[2][0]);
+	return EXIT_SUCCESS;
+}
