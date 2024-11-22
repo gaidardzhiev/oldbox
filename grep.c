0b606dd313ccd045458120b32ba20f35ff7fcee3
gaidardzhiev <1v4n@protonmail.com>
2024-11-22 15:46:02 +0200

    Fri Nov 22 01:46:02 PM UTC 2024

--- grep.c
+++ grep.c
@@ -8,8 +8,7 @@
 #define STDERR "\x63\x6F\x75\x6C\x64\x20\x6E\x6F\x74\x20\x6F\x70\x65\x6E\x20\x66\x69\x6C\x65"
 #define HEX "\x75\x73\x61\x67\x65\x3A\x20\x25\x73\x20\x3C\x70\x61\x74\x74\x65\x72\x6E\x3E\x20\x3C\x66\x69\x6C\x65\x6E\x61\x6D\x65\x3E"
 
-void highlight(const char*line,const char*pattern)
-{
+void highlight(const char*line,const char*pattern) {
 	const char*pos=line;
 	while((pos=strstr(pos,pattern))!=NULL) {
 		fwrite(line,1,pos-line,stdout);
@@ -20,8 +19,7 @@ void highlight(const char*line,const char*pattern)
 	printf("%s",line);
 }
 
-void match(const char*filename,const char*pattern)
-{
+void match(const char*filename,const char*pattern) {
 	FILE*file=fopen(filename,"r");
 	if(!file) {
 		fprintf(stderr,STDERR"\n");
@@ -37,8 +35,7 @@ void match(const char*filename,const char*pattern)
 	fclose(file);
 }
 
-int main(int argc,char*argv[])
-{
+int main(int argc,char*argv[]) {
 	if(argc!=3) {
 		fprintf(stderr,HEX"\n",argv[0]);
 		return EXIT_FAILURE;
