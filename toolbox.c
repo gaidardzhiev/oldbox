1ac44bb82b182e5854d0922625e98eb62992e7bb
gaidardzhiev <1v4n@protonmail.com>
2024-11-25 21:58:00 +0200

    Mon Nov 25 07:57:59 PM UTC 2024

--- toolbox.c
+++ toolbox.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-	const char *dir = "/home/src/1v4n/tools/";
+	const char *dir = "/home/src/1v4n/toolbox/";
 	if (argc < 2) {
 		printf("usage: %s <echo|cat|cp|basename|kill|ln|mount|nice|printf|rev|sleep|sync|tee|touch|tr|true|tty|umount|wc|yes|shell|pwd|ps|grep|du|rm|ascii2hex|hexdump|false|replace|readelf|strings|ls|xoda|id> <options>\n", argv[0]);
 		return 1;
