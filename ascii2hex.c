60f58945121b29aa0820f0fe19ae33642511f6c1
gaidardzhiev <1v4n@protonmail.com>
2024-11-26 17:52:39 +0200

    Tue Nov 26 03:52:38 PM UTC 2024

--- ascii2hex.c
+++ ascii2hex.c
@@ -3,18 +3,18 @@
 
 #define HEX "\x65\x6E\x74\x65\x72\x20\x74\x68\x65\x20\x61\x73\x63\x69\x69\x20\x73\x74\x72\x69\x6E\x67\x3A"
 
-void ascii2hex(const char*input) {
-	for(int i=0; i<strlen(input); i++) {
-		printf("\\x%02X", (unsigned char)input[i]);
+void ascii2hex(const char*in) {
+	for(int i=0; i<strlen(in); i++) {
+		printf("\\x%02X", (unsigned char)in[i]);
 	}
 	printf("\n");
 }
 
 int main() {
-	char input[1024];
+	char in[8094];
 	printf(HEX);
-	fgets(input,sizeof(input),stdin);
-	input[strcspn(input,"\n")]=0;
-	ascii2hex(input);
+	fgets(in,sizeof(in),stdin);
+	in[strcspn(in,"\n")]=0;
+	ascii2hex(in);
 	return 0;
 }
