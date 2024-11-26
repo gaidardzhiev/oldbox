514e80e52ee9277b7fa8c6bd151abbd9d6940380
gaidardzhiev <1v4n@protonmail.com>
2024-11-26 12:04:43 +0200

    Tue Nov 26 10:04:42 AM UTC 2024

--- tty.c
+++ tty.c
@@ -7,7 +7,7 @@ int main(int argc, char **argv) {
 	char*ttyName=ttyname(STDIN_FILENO);
 	if (argc==2&&strcmp(argv[1],"-s")==0) {
 	} else {
-		printf("%s\n", ttyName ? ttyName : "not a tty");
+		printf("%s\n",ttyName?ttyName:"not a tty");
 	}
-	return ttyName ? EXIT_SUCCESS : EXIT_FAILURE;
+	return ttyName?EXIT_SUCCESS:EXIT_FAILURE;
 }
