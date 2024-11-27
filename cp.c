18541d113f6162af40a169ecffb24d53e69ec397
gaidardzhiev <1v4n@protonmail.com>
2024-11-27 11:32:31 +0200

    Wed Nov 27 09:32:30 AM UTC 2024

--- cp.c
+++ cp.c
@@ -5,71 +5,92 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-#define BSIZE 8192
+#define BUF 65536
 
-struct stat stbuf1,stbuf2;
-char iobuf[BSIZE];
-int copy(char*from,char*to);
-int main(argc,argv)char*argv[]; {
-	register i,r;
-	if(argc<3)goto usage;
-	if(argc>3) {
-		if(stat(argv[argc-1], &stbuf2)<0)goto usage;
-		if((stbuf2.st_mode&S_IFMT)!=S_IFDIR)goto usage;
+#define O "\x75\x73\x61\x67\x65\x3A\x20\x63\x70\x20\x3C\x73\x6F\x75\x72\x63\x65\x3E\x20\x3C\x64\x65\x73\x74\x3E"
+#define B "\x63\x70\x3A\x20\x63\x61\x6E\x6E\x6F\x74\x20\x6F\x70\x65\x6E\x20\x25\x73"
+#define F "\x63\x70\x3A\x20\x63\x61\x6E\x6E\x6F\x74\x20\x63\x6F\x70\x79\x20\x66\x69\x6C\x65\x20\x74\x6F\x20\x69\x74\x73\x65\x6C\x66\x2E\x2E\x2E"
+#define U "\x63\x70\x3A\x20\x63\x61\x6E\x6E\x6F\x74\x20\x63\x72\x65\x61\x74\x65\x20\x25\x73"
+#define S "\x63\x70\x3A\x20\x72\x65\x61\x64\x20\x65\x72\x72\x6F\x72\x2E\x2E\x2E"
+#define C "\x63\x70\x3A\x20\x77\x72\x69\x74\x65\x20\x65\x72\x72\x6F\x72\x2E\x2E\x2E"
+
+struct stat ss, ds;
+char b[BUF];
+
+int cf(const char *sp, const char *dp);
+void u();
+
+int main(int argc, char *argv[]) {
+	if (argc < 3) {
+		u();
 	}
-	r=0;
-	for(i=1; i<argc-1; i++)r|=copy(argv[i],argv[argc-1]);
-	exit(r);
-usage:
-	fprintf(stderr,"usage:cp <source> <dest>\n");
-	exit(1);
+	if (argc > 3) {
+		if (stat(argv[argc - 1], &ds) < 0 || (ds.st_mode & S_IFMT) != S_IFDIR) {
+			u();
+		}
+	}
+	int r = 0;
+	for (int i = 1; i < argc - 1; i++) {
+		r |= cf(argv[i], argv[argc - 1]);
+	}
+	return r;
 }
 
-copy(from,to)char*from,*to; {
-	int fold,fnew,n;
-	register char*p1,*p2,*bp;
-	int mode;
-	if((fold=open(from,0))<0) {
-		fprintf(stderr,"cp: cannot open %s\n",from);
+void u() {
+	fprintf(stderr,O"\n");
+	exit(EXIT_FAILURE);
+}
+
+int cf(const char *sp, const char *dp) {
+	int sf = open(sp, O_RDONLY);
+	if (sf < 0) {
+		fprintf(stderr,B"\n", sp);
 		return 1;
 	}
-	fstat(fold, &stbuf1);
-	mode=stbuf1.st_mode;
-	if(stat(to, &stbuf2)>=0&&(stbuf2.st_mode&S_IFMT)==S_IFDIR) {
-		p1=from;
-		p2=to;
-		bp=iobuf;
-		while(*bp++=*p2++);
-		bp[-1]='/';
-		p2=bp;
-		while(*bp=*p1++)if(*bp++=='/')bp=p2;
-		to=iobuf;
+	fstat(sf, &ss);
+	int m = ss.st_mode;
+	if (stat(dp, &ds) >= 0 && (ds.st_mode & S_IFMT) == S_IFDIR) {
+		char *sptr = (char *)sp;
+		char *dptr = (char *)dp;
+		char *bptr = b;
+		while (*bptr++ = *dptr++);
+		bptr[-1] = '/';
+		dptr = bptr;
+		while (*bptr = *sptr++) {
+			if (*bptr++ == '/') {
+				bptr = dptr;
+			}
+		}
+		dp = b;
 	}
-	if(stat(to, &stbuf2)>=0) {
-		if(stbuf1.st_dev==stbuf2.st_dev&&stbuf1.st_ino==stbuf2.st_ino) {
-			fprintf(stderr,"cp: cannot copy file to itself...\n");
+	if (stat(dp, &ds) >= 0) {
+		if (ss.st_dev == ds.st_dev && ss.st_ino == ds.st_ino) {
+			fprintf(stderr,F"\n");
+			close(sf);
 			return 1;
 		}
 	}
-	if((fnew=creat(to,mode))<0) {
-		fprintf(stderr,"cp: cannot create... %s\n",to);
-		close(fold);
+	int df = creat(dp, m);
+	if (df < 0) {
+		fprintf(stderr,U"\n", dp);
+		close(sf);
 		return 1;
 	}
-	while(n=read(fold,iobuf,BSIZE)) {
-		if(n<0) {
-			fprintf(stderr,"cp: read error...\n");
-			close(fold);
-			close(fnew);
-			return(1);
-		} else if(write(fnew,iobuf,n)!=n) {
-			fprintf(stderr,"cp: write error...\n");
-			close(fold);
-			close(fnew);
+	ssize_t br;
+	while ((br = read(sf, b, BUF)) > 0) {
+		if (br < 0) {
+			fprintf(stderr,S"\n");
+			close(sf);
+			close(df);
+			return 1;
+		} else if (write(df, b, br) != br) {
+			fprintf(stderr,C"\n");
+			close(sf);
+			close(df);
 			return 1;
 		}
 	}
-	close(fold);
-	close(fnew);
+	close(sf);
+	close(df);
 	return 0;
 }
