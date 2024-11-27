4342296dfb4ee751bfe477cc11259458419f9bcf
gaidardzhiev <1v4n@protonmail.com>
2024-11-27 20:18:25 +0200

    Wed Nov 27 06:18:24 PM UTC 2024

--- toolbox.c
+++ toolbox.c
@@ -2,21 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {
-	const char *dir = "/home/src/1v4n/toolbox/";
-	if (argc < 2) {
-		printf("usage: %s <echo|cat|cp|basename|kill|ln|mount|nice|printf|rev|sleep|sync|tee|touch|tr|true|tty|umount|wc|yes|shell|pwd|ps|grep|du|rm|ascii2hex|hexdump|false|replace|readelf|strings|ls|xoda|id> <options>\n", argv[0]);
+int main(int c, char *v[]) {
+	const char *d = "/home/src/1v4n/toolbox/";
+	if (c < 2) {
+		printf("usage: %s <echo|cat|cp|basename|kill|ln|mount|nice|printf|rev|sleep|sync|tee|touch|tr|true|tty|umount|wc|yes|shell|pwd|ps|grep|du|rm|ascii2hex|hexdump|false|replace|readelf|strings|ls|xoda|id> <options>\n",v[0]);
 		return 1;
 	}
-	char tool[2048];
-	snprintf (tool, sizeof (tool), "%s/%s", dir, argv[1]);
-	for (int i=2; i < argc; i++) {
-		strcat(tool, " ");
-		strcat(tool, argv[i]);
+	char t[2048];
+	snprintf (t, sizeof (t), "%s/%s", d, v[1]);
+	for (int i=2; i < c; i++) {
+		strcat(t, " ");
+		strcat(t, v[i]);
 	}
-	if (strcmp(argv[1], "printf") == 0) {
-		strcat(tool, " \"\\n\"");
+	if (strcmp(v[1], "printf") == 0) {
+		strcat(t, " \"\\n\"");
 	}
-	system(tool);
+	system(t);
 	return 0;
 }
