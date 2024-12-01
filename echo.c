f80a77d6539b4a78347f030a9f6a204f189d2247
gaidardzhiev <1v4n@protonmail.com>
2024-12-01 23:09:04 +0200

    Sun Dec  1 09:09:04 PM UTC 2024

--- echo.c
+++ echo.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int c, char *v[]) {
-	int nlf=0;
-	if (c>1&&v[1][0]=='-'&&v[1][1]=='n') {
-		nlf++;
-		c--;
+int main(int p, char *v[]) {
+	int n=0;
+	if (p>1&&v[1][0]=='-'&&v[1][1]=='n') {
+		n++;
+		p--;
 		v++;
 	}
-	for (int i=1; i<c; i++) {
+	for (int i=1; i<p; i++) {
 		fputs(v[i],stdout);
-		if (i<c-1) {
+		if (i<p-1) {
 			putchar(' ');
 		}
 	}
-	if (nlf==0) {
+	if (n==0) {
 		putchar('\n');
 	}
 	return 0;
