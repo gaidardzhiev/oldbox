bb3b92fa4de97e58250e63fba363eabd509ebd70
gaidardzhiev <1v4n@protonmail.com>
2024-12-01 23:05:47 +0200

    Sun Dec  1 09:05:47 PM UTC 2024

--- echo.c
+++ echo.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
+int main(int c, char *v[]) {
 	int nlf=0;
-	if (argc>1&&argv[1][0]=='-'&&argv[1][1]=='n') {
+	if (c>1&&v[1][0]=='-'&&v[1][1]=='n') {
 		nlf++;
-		argc--;
-		argv++;
+		c--;
+		v++;
 	}
-	for (int i=1; i<argc; i++) {
-		fputs(argv[i],stdout);
-		if (i<argc-1) {
+	for (int i=1; i<c; i++) {
+		fputs(v[i],stdout);
+		if (i<c-1) {
 			putchar(' ');
 		}
 	}
