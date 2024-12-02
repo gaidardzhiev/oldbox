569c9a09896b2e79c79e76ec2b219dfb5335a265
gaidardzhiev <1v4n@protonmail.com>
2024-12-02 11:50:38 +0200

    Mon Dec  2 09:50:37 AM UTC 2024

--- rev.c
+++ rev.c
@@ -5,11 +5,11 @@
 
 char l[N];
 FILE *in;
-int main(argc,argv)char**argv; {
+int main(z,argv)char**argv; {
 	register i,c;
 	in=stdin;
 	do {
-		if(argc>1) {
+		if(z>1) {
 			if((in=fopen(argv[1],"r"))==NULL) {
 				fprintf(stderr,"rev: cannot open %s\n",argv[1]);
 				exit(1);
@@ -33,7 +33,7 @@ int main(argc,argv)char**argv; {
 		}
 eof:
 		fclose(in);
-		argc--;
+		z--;
 		argv++;
-	} while(argc>1);
+	} while(z>1);
 }
