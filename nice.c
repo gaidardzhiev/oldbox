55400eb62f473136680dfbe51e89e78dd2936a28
gaidardzhiev <1v4n@protonmail.com>
2024-12-03 11:52:17 +0200

    Tue Dec  3 09:52:16 AM UTC 2024

--- nice.c
+++ nice.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/resource.h>
 
-int main(argc,argv)int argc;char*argv[]; {
-	int nicarg=10;
-	extern errno;
-	extern char*sys_errlist[];
-	if(argc>1&&argv[1][0]=='-') {
-		nicarg=atoi(&argv[1][1]);
-		argc--;
-		argv++;
+int main(int z, char *x[]) {
+	int n = 10;
+	if (z > 1 && x[1][0] == '-') {
+		n = atoi(&x[1][1]);
+		z--;
+		x++;
 	}
-	if(argc<2) {
-		fputs("usage: nice <opt> <cmd <arg>...>\n",stderr);
-		exit(1);
+	if (z < 2) {
+		fputs("usage: nice <opt> <cmd <arg>...>>\n", stderr);
+		exit(EXIT_FAILURE);
 	}
-	nice(nicarg);
-	execvp(argv[1], &argv[1]);
-	fprintf(stderr,"%s: %s\n",sys_errlist[errno],argv[1]);
-	exit(1);
+	if (setpriority(PRIO_PROCESS, 0, n) == -1) {
+		perror("failed");
+		exit(EXIT_FAILURE);
+	}
+	execvp(x[1], x + 1);
+	perror(x[1]);
+	exit(EXIT_FAILURE);
 }
