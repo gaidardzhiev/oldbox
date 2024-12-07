b8f50ccf1c834649d4c90911d99e2ed325b57931
gaidardzhiev <1v4n@protonmail.com>
2024-12-07 10:26:00 +0200

    Sat Dec  7 08:26:00 AM UTC 2024

--- touch.c
+++ touch.c
@@ -5,7 +5,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-void t(int f, char *name);
+void t(int f, char *n);
 
 int main(argc,argv)int argc;char*argv[]; {
 	int i;
@@ -14,17 +14,17 @@ int main(argc,argv)int argc;char*argv[]; {
 		else f=0;
 }
 
-void t(f,name)int f;char*name; {
+void t(f,n)int f;char*n; {
 	struct stat stbuff;
 	char junk[1];
 	int fd;
-	if(stat(name, &stbuff)<0)if(f)goto create;
+	if(stat(n, &stbuff)<0)if(f)goto create;
 		else {
-			fprintf(stderr,"touch: file %s does not exist...\n",name);
+			fprintf(stderr,"touch: file %s does not exist...\n",n);
 			return;
 		}
 	if(stbuff.st_size==0)goto create;
-	if((fd=open(name,2))<0)goto bad;
+	if((fd=open(n,2))<0)goto bad;
 	if(read(fd,junk,1)<1) {
 		close(fd);
 		goto bad;
@@ -37,9 +37,9 @@ void t(f,name)int f;char*name; {
 	close(fd);
 	return;
 bad:
-	fprintf(stderr,"cannot touch %s\n",name);
+	fprintf(stderr,"cannot touch %s\n",n);
 	return;
 create:
-	if((fd=creat(name,0666))<0)goto bad;
+	if((fd=creat(n,0666))<0)goto bad;
 	close(fd);
 }
