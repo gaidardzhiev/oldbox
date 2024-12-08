c0ca15eed4ce69f711fdaf1c4fb5f17a7c5e24fc
gaidardzhiev <1v4n@protonmail.com>
2024-12-08 11:40:27 +0200

    Sun Dec  8 09:40:27 AM UTC 2024

---
+++ tree.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <string.h>
+
+void t(const char *b, char *p) {
+	struct dirent *dp;
+	DIR *d = opendir(b);
+	if (!d) {
+		return;
+	}
+	while ((dp = readdir(d)) != NULL) {
+		if (strcmp(dp->d_name, ".") == 0 || strcmp(dp->d_name, "..") == 0) {
+			continue;
+		}
+		printf("%s|____%s\n", p, dp->d_name);
+		char a[1024];
+		snprintf(a, sizeof(a), "%s/%s", b, dp->d_name);
+		if (dp->d_type == DT_DIR) {
+			strcat(p, "   ");
+			t(a, p);
+			p[strlen(p) - 3] = '\0';
+		}
+	}
+	closedir(d);
+}
+
+int main(int z, char *x[]) {
+	const char *a = (z > 1) ? x[1] : ".";
+	printf("%s\n",a);
+	char p[1024] = "";
+	t(a, p);
+	return 0;
+}
