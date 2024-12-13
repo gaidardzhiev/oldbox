125408e5cae29e0a73cafdfb70b0802552f77281
gaidardzhiev <1v4n@protonmail.com>
2024-12-13 11:34:58 +0200

    Fri Dec 13 09:34:58 AM UTC 2024

--- readelf.c
+++ readelf.c
@@ -4,36 +4,35 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-void print_elf_header(Elf64_Ehdr *header) {
+void elf(Elf64_Ehdr *h) {
 	printf("ELF header:\n");
 	printf("  magic:   %x %x %x %x %x %x %x %x %x %x %x %x %x %x %x %x\n",
-	       header->e_ident[0], header->e_ident[1], header->e_ident[2], header->e_ident[3],
-	       header->e_ident[4], header->e_ident[5], header->e_ident[6], header->e_ident[7],
-	       header->e_ident[8], header->e_ident[9], header->e_ident[10], header->e_ident[11],
-	       header->e_ident[12], header->e_ident[13], header->e_ident[14], header->e_ident[15]);
-	printf("  type:                             %u\n", header->e_type);
-	printf("  machine:                          %u\n", header->e_machine);
-	printf("  version:                          %u\n", header->e_version);
-	printf("  entry point address:              0x%lx\n", header->e_entry);
-	printf("  start of program headers:         %u (bytes into file)\n", header->e_phoff);
-	printf("  start of section headers:         %u (bytes into file)\n", header->e_shoff);
-	printf("  flags:                            %u\n", header->e_flags);
-	printf("  size of this header:              %u (bytes)\n", header->e_ehsize);
-	printf("  size of program headers:           %u (bytes)\n", header->e_phentsize);
-	printf("  number of program headers:         %u\n", header->e_phnum);
-	printf("  size of section headers:           %u (bytes)\n", header->e_shentsize);
-	printf("  number of section headers:         %u\n", header->e_shnum);
-	printf("  section header string table index: %u\n", header->e_shstrndx);
+	       h->e_ident[0], h->e_ident[1], h->e_ident[2], h->e_ident[3],
+	       h->e_ident[4], h->e_ident[5], h->e_ident[6], h->e_ident[7],
+	       h->e_ident[8], h->e_ident[9], h->e_ident[10], h->e_ident[11],
+	       h->e_ident[12], h->e_ident[13], h->e_ident[14], h->e_ident[15]);
+	printf("  type:                             %u\n", h->e_type);
+	printf("  machine:                          %u\n", h->e_machine);
+	printf("  version:                          %u\n", h->e_version);
+	printf("  entry point address:              0x%lx\n", h->e_entry);
+	printf("  start of program headers:         %u (bytes into file)\n", h->e_phoff);
+	printf("  start of section headers:         %u (bytes into file)\n", h->e_shoff);
+	printf("  flags:                            %u\n", h->e_flags);
+	printf("  size of this header:              %u (bytes)\n", h->e_ehsize);
+	printf("  size of program headers:           %u (bytes)\n", h->e_phentsize);
+	printf("  number of program headers:         %u\n", h->e_phnum);
+	printf("  size of section headers:           %u (bytes)\n", h->e_shentsize);
+	printf("  number of section headers:         %u\n", h->e_shnum);
+	printf("  section header string table index: %u\n", h->e_shstrndx);
 }
 
-//void print_section_headers(int fd, Elf64_Ehdr *header)
-//{
-//	Elf64_Shdr section_header;
-//	lseek(fd, header->e_shoff, SEEK_SET);
+//void psh(int fd, Elf64_Ehdr *h) {
+//	Elf64_Shdr sh;
+//	lseek(fd, h->e_shoff, SEEK_SET);
 //	printf("\nsection headers:\n");
-//	for (int i = 0; i < header->e_shnum; i++) {
-//		read(fd, &section_header, sizeof(section_header));
-//		printf("  [%2d] %20s\n", i, section_header.sh_name);
+//	for (int i = 0; i < h->e_shnum; i++) {
+//		read(fd, &sh, sizeof(sh));
+//		printf("  [%2d] %20s\n", i, sh.sh_name);
 //	}
 //}
 
@@ -47,16 +46,16 @@ int main(int argc, char *argv[]) {
 		perror("error opening file");
 		return EXIT_FAILURE;
 	}
-	Elf64_Ehdr header;
-	read(fd, &header, sizeof(header));
-	if (header.e_ident[0] != ELFMAG0 || header.e_ident[1] != ELFMAG1 ||
-	    header.e_ident[2] != ELFMAG2 || header.e_ident[3] != ELFMAG3) {
+	Elf64_Ehdr h;
+	read(fd, &h, sizeof(h));
+	if (h.e_ident[0] != ELFMAG0 || h.e_ident[1] != ELFMAG1 ||
+	    h.e_ident[2] != ELFMAG2 || h.e_ident[3] != ELFMAG3) {
 		fprintf(stderr, "not a valid ELF file\n");
 		close(fd);
 		return EXIT_FAILURE;
 	}
-	print_elf_header(&header);
-//	print_section_headers(fd, &header);
+	elf(&h);
+//	psh(fd, &header);
 	close(fd);
 	return EXIT_SUCCESS;
 }
