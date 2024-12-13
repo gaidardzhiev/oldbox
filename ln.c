b9f6fccd636fdf08a4b1b47e67d0f22d9fe63cdd
gaidardzhiev <1v4n@protonmail.com>
2024-12-13 11:22:58 +0200

    Fri Dec 13 09:22:58 AM UTC 2024

--- ln.c
+++ ln.c
@@ -5,42 +5,49 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-char*rindex();
-int main(argc,argv)char**argv; {
-	struct stat statb;
-	register char*np;
-	int fflag=0;
-	char nb[100],*name=nb,*arg2;
-	int statres;
-	if(argc>1&&strcmp(argv[1],"-f")==0) {
-		argc--;
-		argv++;
-		fflag++;
+char* f(const char* s, char c) {
+	char* l = NULL;
+	while (*s) {
+		if (*s == c) {
+			l = (char*)s;
+		}
+		s++;
 	}
-	if(argc<2||argc>3) {
-		printf("usage: ln <target> <link>\n");
-		exit(1);
+	return l;
+}
+
+int main(int z, char** x) {
+	struct stat fs;
+	char n[100], *b, *t;
+	int ff = 0;
+	if (z > 1 && strcmp(x[1], "-f") == 0) {
+		z--;
+		x++;
+		ff = 1;
 	}
-	np=rindex(argv[1],'/');
-	if(np==0)np=argv[1];
-	else np++;
-	if(argc==2)arg2=np;
-	else arg2=argv[2];
-	statres=stat(argv[1], &statb);
-	if(statres<0) {
-		printf("ln: %s does not exist\n",argv[1]);
-		exit(1);
+	if (z < 2 || z > 3) {
+		fprintf(stderr, "usage: ln <target> <link>\n");
+		exit(EXIT_FAILURE);
 	}
-	if(fflag==0&&(statb.st_mode&S_IFMT)==S_IFDIR) {
-		printf("ln: %s is a directory\n",argv[1]);
-		exit(1);
+	b = f(x[1], '/');
+	b = (b == NULL) ? x[1] : b + 1;
+	t = (z == 2) ? b : x[2];
+	if (stat(x[1], &fs) < 0) {
+		fprintf(stderr, "ln: %s does not exist\n", x[1]);
+		exit(EXIT_FAILURE);
 	}
-	statres=stat(arg2, &statb);
-	if(statres>=0&&(statb.st_mode&S_IFMT)==S_IFDIR)sprintf(name,"%s/%s",arg2,np);
-	else name=arg2;
-	if(link(argv[1],name)<0) {
+	if (!ff && (fs.st_mode & S_IFMT) == S_IFDIR) {
+		fprintf(stderr, "ln: %s is a directory\n", x[1]);
+		exit(EXIT_FAILURE);
+	}
+	if (stat(t, &fs) >= 0 && (fs.st_mode & S_IFMT) == S_IFDIR) {
+		snprintf(n, sizeof(n), "%s/%s", t, b);
+	} else {
+		strncpy(n, t, sizeof(n));
+	}
+	if (link(x[1], n) < 0) {
 		perror("ln");
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
-	exit(0);
+	return EXIT_SUCCESS;
 }
