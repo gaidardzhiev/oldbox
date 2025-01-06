cd12a53318433875cdcfe682e620f60c5d2307f9
gaidardzhiev <1v4n@protonmail.com>
2025-01-06 10:31:19 +0200

    Mon Jan  6 08:31:19 AM UTC 2025

---
+++ magic.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define q 16
+
+void s(const char *p) {
+	FILE *f = fopen(p, "rb");
+	if (!f) {
+		perror("error opening file");
+		return;
+	}
+	unsigned char m[q];
+	size_t b = fread(m, 1, q, f);
+	printf("magic signature bytes: ");
+	for (size_t i = 0; i < q; i++) {
+		printf("%02X ", m[i]);
+	}
+	printf("\n");
+	printf("human readable ASCII: ");
+	for (size_t i = 0; i < q; i++) {
+		if (m[i] >= 32 && m[i] <= 126) {
+			printf("%c", m[i]);
+		} else {
+			printf(".");
+		}
+	}
+	printf("\n");
+	fclose(f);
+}
+
+int main(int z, char *x[]) {
+	if (z != 2) {
+		fprintf(stderr, "usage: %s <file>\n", x[0]);
+		return 1;
+	}
+	s(x[1]);
+	return 0;
+}
