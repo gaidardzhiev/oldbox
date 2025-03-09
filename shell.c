9e080b6c5366ba4896815e97f6093864c2603c1b
gaidardzhiev <1v4n@protonmail.com>
2025-03-09 19:43:48 +0000

    Sun Mar  9 07:43:48 PM UTC 2025

--- shell.c
+++ shell.c
@@ -38,7 +38,8 @@ void x(char*c) {
 int main() {
 	char c[MCL];
 	while(1) {
-		printf("shell# ");
+//		printf("shell# ");
+		printf("%s# ", getpwuid(getuid())->pw_name);
 		if(fgets(c,sizeof(c),stdin)==NULL) {
 			break;
 		}
