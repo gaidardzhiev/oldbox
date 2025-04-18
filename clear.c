f823f8d287beb56e3a6ea348ad1c07c07218e0ee
gaidardzhiev <1v4n@protonmail.com>
2025-04-18 08:03:21 +0000

    Fri Apr 18 08:03:21 AM UTC 2025

--- clear.c
+++ clear.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #define O 1
-#define I int
-#define C char
-#define R return
-#define P putchar
-typedef I i;typedef C c;
-I main(){c*s="\033[2J\033[H";i j=O;O_:P(s[j-1]);j++;if(s[j-2])goto O_;R O;}
+#define B int
+#define F char
+#define U return
+#define S putchar
+
+typedef B i;
+typedef F c;
+B main() {
+	c*s="\033[2J\033[H";
+	i j=O;
+	O_:
+	S(s[j-1]);
+	j++;
+	if(s[j-2])
+		goto O_;
+	U O;
+}
